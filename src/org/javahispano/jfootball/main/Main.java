f586521b6560b8d9e6a423303d8922c22a87a14b
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2016-02-02 15:02:14 +0100

    First commit

---
+++ src/org/javahispano/jfootball/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.javahispano.jfootball.main;
+
+/**
+ * @author adou
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
