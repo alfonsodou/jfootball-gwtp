bdf46c6340ee45386b7b1e7e1d77aa3c8d7083c5
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2016-05-26 14:55:19 +0200

    Cambios en basegameentity

--- src/main/java/org/javahispano/jfootball/framework/entity/BaseGameEntity.java
+++ src/main/java/org/javahispano/jfootball/framework/entity/BaseGameEntity.java
@@ -30,5 +30,100 @@ public class BaseGameEntity {
 	 * the magnitude of this object's bounding radius
 	 */
 	private double boundingRadius;
+	/*
+	 * this is the next valid ID. Each time a BaseGameEntity is instantiated
+  	 * this value is updated
+	 */
+	private static int  nextValidID;
 
+	public BaseGameEntity(int id) {
+		setId(id);
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+		BaseGameEntity.nextValidID = id + 1;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public EntityType getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(EntityType type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the pos
+	 */
+	public Vector2D getPos() {
+		return pos;
+	}
+
+	/**
+	 * @param pos the pos to set
+	 */
+	public void setPos(Vector2D pos) {
+		this.pos = pos;
+	}
+
+	/**
+	 * @return the scale
+	 */
+	public Vector2D getScale() {
+		return scale;
+	}
+
+	/**
+	 * @param scale the scale to set
+	 */
+	public void setScale(Vector2D scale) {
+		this.scale = scale;
+	}
+
+	/**
+	 * @return the boundingRadius
+	 */
+	public double getBoundingRadius() {
+		return boundingRadius;
+	}
+
+	/**
+	 * @param boundingRadius the boundingRadius to set
+	 */
+	public void setBoundingRadius(double boundingRadius) {
+		this.boundingRadius = boundingRadius;
+	}
+
+	/**
+	 * @return the nextValidID
+	 */
+	public static int getNextValidID() {
+		return nextValidID;
+	}
+
+	/**
+	 * @param nextValidID the nextValidID to set
+	 */
+	public static void setNextValidID(int nextValidID) {
+		BaseGameEntity.nextValidID = nextValidID;
+	}
+	
+	
 }
